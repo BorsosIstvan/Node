d56b2561ca4261b17aa43c48c2b536d0e0e54f65
BorsosIstvan <bistvanus@gmail.com>
2022-03-11 18:16:11 +0200

    Hello login

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ console.log('Hello word');
 const http = require('http');
 
 const requestListener = function(req, res) {
+
+/*
+Request:
+    URL pl http://poci.n-soft.net:8080
+    METHOD pl GET, POST, DELETE, PATCH, PUT
+    QUERY parameterek 
+    BODY 
+    HEADER pl "content-type: application/json"
+
+Response:
+    HEADER
+    BODY
+    STATUS pl 200
+*/
+
+switch(true){
+    case req.url === '/' && req.method === 'GET':
+        res.setHeader('content-type', 'text/html', 'charset = utf-8');
+        res.writeHead(200);
+        res.end('Cimlap <a href="/login">Bejelentkezes</a>');
+    break
+    case req.url == '/login' && req.method === 'GET':
+        res.setHeader('content-type', 'text/html', 'charset = utf-8');
+        res.writeHead(200);
+        res.end('Bejelentkezes <a href="/">Cimlap</a>');
+    break
+
+}
+
+
+
     res.writeHead(200);
     res.end('Hello, World!');
 }
