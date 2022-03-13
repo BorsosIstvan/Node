bf955320d8d04bd1b07706aac4f03a2bd68f5d4a
BorsosIstvan <bistvanus@gmail.com>
2022-03-13 22:27:18 +0200

    render

--- public/script.js
+++ public/script.js
@@ -3,5 +3,14 @@ document.getElementById('fetch-qr').onclick = fetchAndRenderQrs;
 async function fetchAndRenderQrs(){
     const response = await fetch('/qrcodes.json');
     const qrcodes = await response.json();
-    console.log(qrcodes);
+    
+    let qrHTML = "<h1>Qr code list:</h1>";
+    for (const qr of qrcodes){
+        qrHTML +=`
+        <p>${qr.name}</p>
+        <p>${qr.description}</p>
+        `
+    }
+
+    document.getElementById('qr-list').innerHTML = qrHTML;
 }
